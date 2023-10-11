a45f4b2624b517398be5d81cdb1908f329412f57
Victor Jouffrey <vicatjou@gmail.com>
2023-10-11 17:59:56 +0200

    Fix variables api

--- src/repositories/variables/service.rs
+++ src/repositories/variables/service.rs
@@ -7,24 +7,29 @@ use super::entities::{BoolMessageResponse, StringMessageResponse};
 
 pub async fn get_flyingduck_enabled() -> Result<bool> {
     Ok(get_hopsworks_client()
-            .await
-            .request(Method::GET, "variables/enable_flying_duck", true, true)
-            .await?
-            .send()
-            .await?
-            .json::<BoolMessageResponse>()
-            .await?
-            .success_message)
+        .await
+        .request(Method::GET, "variables/enable_flying_duck", true, false)
+        .await?
+        .send()
+        .await?
+        .json::<BoolMessageResponse>()
+        .await?
+        .success_message)
 }
 
 pub async fn get_loadbalancer_external_domain() -> Result<String> {
     Ok(get_hopsworks_client()
-            .await
-            .request(Method::GET, "variables/loadbalancer_external_domain", true, true)
-            .await?
-            .send()
-            .await?
-            .json::<StringMessageResponse>()
-            .await?
-            .success_message)
+        .await
+        .request(
+            Method::GET,
+            "variables/loadbalancer_external_domain",
+            true,
+            false,
+        )
+        .await?
+        .send()
+        .await?
+        .json::<StringMessageResponse>()
+        .await?
+        .success_message)
 }
