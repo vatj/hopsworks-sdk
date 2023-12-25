c977eb4a3ee519eabab5c7f9b3df4509b0747965
Victor Jouffrey <vicatjou@gmail.com>
2023-12-25 20:31:31 +0100

    Add extra docs for filter from features

--- src/feature_store/feature_group/feature.rs
+++ src/feature_store/feature_group/feature.rs
@@ -71,6 +71,28 @@ impl Feature {
         Ok(QueryFilter::new_like(pattern, self.clone())?.into())
     }
 
+    /// Create a new filter for this feature. Often used in combination with a query,
+    /// to create a new feature view including only the features matching the filter.
+    ///
+    /// # Arguments
+    /// * `pattern` - The pattern to match.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use color_eyre::Result;
+    /// # use hopsworks_rs::hopsworks_login;
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<()> {
+    /// let feature_group = hopsworks_login(None).await?
+    ///   .get_feature_store().await?
+    ///   .get_feature_group("demo_feature_group", Some(1)).await?
+    ///   .expect("Feature group not found");
+    ///
+    /// let mut query = feature_group.select(&["feature_1", "feature_2"])?;
+    /// query.add_filters(vec![feature_group.get_feature("feature_1").expect("feature_1 not found").filter_in(vec!["Alice", "Bob"])?]);
+    /// # Ok(())
+    /// # }
     pub fn filter_in<T>(&self, values: Vec<T>) -> Result<QueryFilterOrLogic>
     where
         T: PartialEq + serde::Serialize + serde::de::DeserializeOwned,
@@ -78,6 +100,28 @@ impl Feature {
         Ok(QueryFilter::new_in(values, self.clone())?.into())
     }
 
+    /// Create a new filter for this feature. Often used in combination with a query,
+    /// to create a new feature view including only the features matching the filter.
+    ///
+    /// # Arguments
+    /// * `pattern` - The pattern to match.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use color_eyre::Result;
+    /// # use hopsworks_rs::hopsworks_login;
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<()> {
+    /// let feature_group = hopsworks_login(None).await?
+    ///   .get_feature_store().await?
+    ///   .get_feature_group("demo_feature_group", Some(1)).await?
+    ///   .expect("Feature group not found");
+    ///
+    /// let mut query = feature_group.select(&["feature_1", "feature_2"])?;
+    /// query.add_filters(vec![feature_group.get_feature("feature_1").expect("feature_1 not found").filter_neq("Deleted")?]);
+    /// # Ok(())
+    /// # }
     pub fn filter_neq<'a, T>(&self, value: T) -> Result<QueryFilterOrLogic>
     where
         T: 'a + PartialEq + serde::Serialize + serde::de::DeserializeOwned,
@@ -88,6 +132,28 @@ impl Feature {
         )
     }
 
+    /// Create a new filter for this feature. Often used in combination with a query,
+    /// to create a new feature view including only the features matching the filter.
+    ///
+    /// # Arguments
+    /// * `pattern` - The pattern to match.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use color_eyre::Result;
+    /// # use hopsworks_rs::hopsworks_login;
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<()> {
+    /// let feature_group = hopsworks_login(None).await?
+    ///   .get_feature_store().await?
+    ///   .get_feature_group("demo_feature_group", Some(1)).await?
+    ///   .expect("Feature group not found");
+    ///
+    /// let mut query = feature_group.select(&["feature_1", "feature_2"])?;
+    /// query.add_filters(vec![feature_group.get_feature("feature_1").expect("feature_1 not found").filter_eq("Active")?]);
+    /// # Ok(())
+    /// # }
     pub fn filter_eq<'a, T>(&self, value: T) -> Result<QueryFilterOrLogic>
     where
         T: 'a + PartialEq + serde::Serialize + serde::de::DeserializeOwned,
@@ -104,6 +170,28 @@ impl Feature {
         )
     }
 
+    /// Create a new filter for this feature. Often used in combination with a query,
+    /// to create a new feature view including only the features matching the filter.
+    ///
+    /// # Arguments
+    /// * `pattern` - The pattern to match.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use color_eyre::Result;
+    /// # use hopsworks_rs::hopsworks_login;
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<()> {
+    /// let feature_group = hopsworks_login(None).await?
+    ///   .get_feature_store().await?
+    ///   .get_feature_group("demo_feature_group", Some(1)).await?
+    ///   .expect("Feature group not found");
+    ///
+    /// let mut query = feature_group.select(&["feature_1", "feature_2"])?;
+    /// query.add_filters(vec![feature_group.get_feature("feature_1").expect("feature_1 not found").filter_gte(3.)?]);
+    /// # Ok(())
+    /// # }
     pub fn filter_gte<'a, T>(&self, value: T) -> Result<QueryFilterOrLogic>
     where
         T: 'a + PartialOrd + serde::Serialize + serde::de::DeserializeOwned,
@@ -116,6 +204,28 @@ impl Feature {
         .into())
     }
 
+    /// Create a new filter for this feature. Often used in combination with a query,
+    /// to create a new feature view including only the features matching the filter.
+    ///
+    /// # Arguments
+    /// * `pattern` - The pattern to match.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use color_eyre::Result;
+    /// # use hopsworks_rs::hopsworks_login;
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<()> {
+    /// let feature_group = hopsworks_login(None).await?
+    ///   .get_feature_store().await?
+    ///   .get_feature_group("demo_feature_group", Some(1)).await?
+    ///   .expect("Feature group not found");
+    ///
+    /// let mut query = feature_group.select(&["feature_1", "feature_2"])?;
+    /// query.add_filters(vec![feature_group.get_feature("feature_1").expect("feature_1 not found").filter_lt(3.)?]);
+    /// # Ok(())
+    /// # }
     pub fn filter_lt<'a, T>(&self, value: T) -> Result<QueryFilterOrLogic>
     where
         T: 'a + PartialOrd + serde::Serialize + serde::de::DeserializeOwned,
@@ -126,6 +236,28 @@ impl Feature {
         )
     }
 
+    /// Create a new filter for this feature. Often used in combination with a query,
+    /// to create a new feature view including only the features matching the filter.
+    ///
+    /// # Arguments
+    /// * `pattern` - The pattern to match.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use color_eyre::Result;
+    /// # use hopsworks_rs::hopsworks_login;
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<()> {
+    /// let feature_group = hopsworks_login(None).await?
+    ///   .get_feature_store().await?
+    ///   .get_feature_group("demo_feature_group", Some(1)).await?
+    ///   .expect("Feature group not found");
+    ///
+    /// let mut query = feature_group.select(&["feature_1", "feature_2"])?;
+    /// query.add_filters(vec![feature_group.get_feature("feature_1").expect("feature_1 not found").filter_lte(3.)?]);
+    /// # Ok(())
+    /// # }
     pub fn filter_lte<'a, T>(&self, value: T) -> Result<QueryFilterOrLogic>
     where
         T: 'a + PartialOrd + serde::Serialize + serde::de::DeserializeOwned,
