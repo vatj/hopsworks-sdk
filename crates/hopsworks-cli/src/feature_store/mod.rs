5d00c4883b6a0e175f20de40a7029a5f29e48a81
Victor Jouffrey <vicatjou@gmail.com>
2024-06-05 11:20:32 +0200

    clean up cli

---
+++ crates/hopsworks-cli/src/feature_store/mod.rs
@@ -0,0 +1,5 @@
+pub mod feature_group;
+pub mod feature_view;
+
+pub use feature_group::FeatureGroupSubCommand;
+pub use feature_view::FeatureViewSubCommand;
--- crates/hopsworks-cli/src/platform/mod.rs
+++ crates/hopsworks-cli/src/platform/mod.rs
@@ -1,2 +1,5 @@
 pub mod job;
 pub mod project;
+
+pub use job::JobSubCommand;
+pub use project::ProjectSubCommand;
