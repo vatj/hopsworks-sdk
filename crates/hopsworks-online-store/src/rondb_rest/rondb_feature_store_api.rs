cf1eaea62a5a4726fbe2bf0c258c8f75706401b1
Victor Jouffrey <vicatjou@gmail.com>
2024-06-24 23:44:07 +0200

    Add skeleton for RonDB REST Client

---
+++ crates/hopsworks-online-store/src/rondb_rest/rondb_feature_store_api.rs
@@ -0,0 +1,28 @@
+use color_eyre::Result;
+use reqwest::Method;
+
+use crate::rondb_rest::get_rondb_rest_client;
+use super::{entities::{BatchFeatureVectors, SingleFeatureVector}, payload::{BatchEntriesPayload, SingleEntryPayload}};
+
+pub async fn ping_rondb_rest_server() -> Result<()> {
+    get_rondb_rest_client().await.request(Method::GET, "ping").await?.send().await?;
+    Ok(())
+}
+
+pub async fn get_single_feature_vector(payload: SingleEntryPayload) -> Result<SingleFeatureVector> {
+    let resp = get_rondb_rest_client().await.request(Method::POST, "feature_store").await?.send();
+
+    match resp {
+        Ok(_) => Ok(SingleFeatureVector {}),
+        _ => todo!()
+    }
+}
+
+pub async fn get_batch_feature_vectors(payload: BatchEntriesPayload) -> Result<BatchFeatureVectors> {
+    let resp = get_rondb_rest_client().await.request(Method::POST, "batch_feature_vector").json().await?.send();
+
+    match resp {
+        Ok(_) => Ok(BatchFeatureVectors {}),
+        _ => todo!()
+    }
+}
