ea2f76b7b5c085a0b55b5721ee23d21ec10fe05b
Victor Jouffrey <vicatjou@gmail.com>
2024-08-25 22:16:37 +0200

    Cleanup some dependencies

--- crates/hopsworks-api/src/online_store/read_rest.rs
+++ crates/hopsworks-api/src/online_store/read_rest.rs
@@ -2,7 +2,7 @@ use color_eyre::Result;
 
 use hopsworks_core::controller::platform::variables::get_loadbalancer_external_domain;
 
-async fn init_online_store_rest_client(api_key: Option<&str>, api_version: Option<&str>, reqwest_client: Option<reqwest::client>) -> Result<()> {
-    let url = get_loadbalancer_external_domain(("online_store_rest_server")).await?;
+async fn init_online_store_rest_client(api_key: Option<&str>, api_version: Option<&str>, reqwest_client: Option<reqwest::Client>) -> Result<()> {
+    let url = get_loadbalancer_external_domain("online_store_rest_server").await?;
     todo!()
 }
