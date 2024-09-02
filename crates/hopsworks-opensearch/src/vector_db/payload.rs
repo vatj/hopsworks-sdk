fc3a4a3163af92d78b880c6ef31c201bd74880d1
Victor Jouffrey <vicatjou@gmail.com>
2024-09-02 10:40:42 +0200

    Add a draft opensearch get_feature_vector method

--- crates/hopsworks-opensearch/src/vector_db/payload.rs
+++ crates/hopsworks-opensearch/src/vector_db/payload.rs
@@ -1,19 +1,21 @@
 use color_eyre::Result;
 
+use hopsworks_core::controller::feature_store::query;
 use serde_json::json;
 
 #[tracing::instrument]
 pub fn build_find_neighbor_opensearch_query(
     size_neighbor_list: u32,
-    col_name: &str, 
-    embedding: &str, 
-    sources: Vec<serde_json::Value>, 
-    filters: Vec<serde_json::Value>) -> Result<serde_json::Value> {
+    col_name: &str,
+    embedding: &str,
+    sources: Vec<serde_json::Value>,
+    filters: Vec<serde_json::Value>,
+) -> Result<serde_json::Value> {
     let mut must_list = Vec::with_capacity(filters.len() + 2);
     must_list.push(json!({ "knn": {col_name: { "vector": embedding, "k": size_neighbor_list } } }));
     must_list.push(json!({ "exists": { "field": col_name } }));
     must_list.extend(filters);
-    
+
     let query = json!({
         "size": size_neighbor_list,
         "query": {
@@ -30,3 +32,38 @@ pub fn build_find_neighbor_opensearch_query(
     Ok(query)
 }
 
+#[tracing::instrument]
+pub fn build_fg_show_query(n_show: u32, primary_keys: &[String]) -> Result<serde_json::Value> {
+    let query = json!({
+        "size": n_show,
+        "query": {
+            "bool": {
+                "must": [
+                    { "exists": { "field": primary_keys } }
+                ]
+            }
+        },
+        "_source": primary_keys
+    });
+
+    Ok(query)
+}
+
+#[tracing::instrument]
+pub fn build_get_feature_vectors_query(
+    n_entries: u32,
+    entries: Vec<serde_json::Value>,
+) -> Result<serde_json::Value> {
+    let query = json!({
+        "size": n_entries,
+        "query": {
+            "bool": {
+                "must": [
+                    { "match": entries }
+                ]
+            }
+        }
+    });
+
+    Ok(query)
+}
--- crates/hopsworks-opensearch/src/vector_db/service.rs
+++ crates/hopsworks-opensearch/src/vector_db/service.rs
@@ -3,7 +3,10 @@ use opensearch::SearchParts;
 
 use crate::get_hopsworks_opensearch_client;
 
-async fn find_neighbors(embedding_index: &str, knn_query: serde_json::Value) -> Result<Vec<serde_json::Value>> {
+async fn find_neighbors(
+    embedding_index: &str,
+    knn_query: serde_json::Value,
+) -> Result<Vec<serde_json::Value>> {
     let client = get_hopsworks_opensearch_client()?;
     let response = client
         .search(SearchParts::Index(&[embedding_index]))
@@ -17,6 +20,27 @@ async fn find_neighbors(embedding_index: &str, knn_query: serde_json::Value) -> 
             let hits = body["hits"]["hits"].as_array().unwrap();
             Ok(hits.to_owned())
         }
-        false => todo!()
+        false => todo!(),
+    }
+}
+
+async fn get_feature_vectors(
+    embedding_index: &str,
+    get_feature_vectors_query: serde_json::Value,
+) -> Result<serde_json::Value> {
+    let client = get_hopsworks_opensearch_client()?;
+    let response = client
+        .search(SearchParts::Index(&[embedding_index]))
+        .body(get_feature_vectors_query)
+        .send()
+        .await?;
+
+    match response.status_code().is_success() {
+        true => {
+            let body = response.json::<serde_json::Value>().await?;
+            let hits = body["hits"]["hits"].as_array().unwrap();
+            Ok(hits[0].to_owned())
+        }
+        false => todo!(),
     }
 }
